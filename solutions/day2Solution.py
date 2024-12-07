8a6ce9d7ac5c9c93d9dc4f3b658fffe995e53936
dawnables <testechxzz@gmail.com>
2024-12-06 23:11:38 -0500

    death

--- solutions/day2Solution.py
+++ solutions/day2Solution.py
@@ -3,27 +3,7 @@ import numpy as np
 
 
 def part_one(file_path):
-
-    with open(file_path) as file:
-        lines = file.readlines()
-
-    safe_count = 0
-    for line in lines:
-        # check for lines that arent continuous one direction (heehee, lazy)
-        levels = [int(level) for level in line.split(' ')]
-        if levels != sorted(levels) and levels != sorted(levels, reverse=True):
-            print('bad line:', line)
-            continue
-        else:
-            # check that numbers are not too far apart or too close
-            for i, curr_num in enumerate(levels):
-                if i == len(levels) - 1:
-                    safe_count += 1
-                elif abs(curr_num - levels[i + 1]) > 3 or abs(curr_num - levels[i + 1]) < 1:
-                    print('Bad line:', line)
-                    break
-
-    return safe_count
+    print('B)')
 
 
 def part_two(file_path):
